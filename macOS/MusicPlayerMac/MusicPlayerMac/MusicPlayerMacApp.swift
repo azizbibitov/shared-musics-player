5ceda9de43571f25418bf700cb7357554573f7d2
Aziz Bibitov <azizbibitov11@gmail.com>
2026-04-23 05:18:10 +0500

    added macos app

---
+++ macOS/MusicPlayerMac/MusicPlayerMac/MusicPlayerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerMacApp.swift
+//  MusicPlayerMac
+//
+//  Created by Aziz Bibitov on 23.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
